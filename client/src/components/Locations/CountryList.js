22f9d58ce9025229a7e79b5500aff8f40eb69980
vpilipets-3 <vpilipets@agilevision.io>
2020-04-06 18:19:19 +0300

    navigation update

--- client/src/components/Locations/CountryList.js
+++ client/src/components/Locations/CountryList.js
@@ -1,53 +1,89 @@
 import React from 'react';
 import { Tabs, Tab } from '@material-ui/core'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
 import Loading from '../Loader/Loading'
+import TextField from '@material-ui/core/TextField';
+
+const styles = {
+  Country: {
+    padding: 20,
+    marginRight: 20,
+    marginTop: 10
+  },
+  City: {
+    padding: 20,
+    marginRight: 20,
+    marginTop: 70
+  }
+}
 
 export default class FetchData extends React.Component {
+
+ 
 
   state = {
     loading: true,
     countries: [],
+    cities: [],
   };
 
   async clickHandler(index) {
     const url = `http://localhost:5000/api/citiesIn/${index}`;
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
+    this.setState({ cities: data })
   }
 
- async componentDidMount() {
+  async componentDidMount() {
     const url = "http://localhost:5000/api/showCountries/";
     const response = await fetch(url);
     const data = await response.json();
     console.log(data);
-    this.setState({countries: data, loading: false});
+    this.setState({ countries: data, loading: false });
   }
 
   render() {
 
-    if(this.state.loading) {
+    if (this.state.loading) {
       return <div>
-        <Loading></Loading>
+        <Loading />
       </div>
     }
 
-    if(!this.state.countries.length) {
+    if (!this.state.countries.length) {
       return <div>No data</div>
     }
 
     return (
-      <div>
-         {this.state.countries.map(country=> (
-           <Tabs orientation="vertical"
-           variant="scrollable"
-           onChange={null}
-           >
-          <Tab label ={country.name} key={country._id } onClick={()=>this.clickHandler(country._id)}>
-          </Tab>
-          </Tabs>
-         ))};
-    </div>
+      <Grid container>
+        <Grid item lg={3} xs={6}>
+          <Paper style={styles.Country} elevation={3}>
+            <Tabs orientation="vertical"
+              variant="scrollable"
+              onChange={null}
+              value={0}
+            >
+              {this.state.countries.map(country => (
+                <Tab label={country.name} key={country._id} onClick={() => this.clickHandler(country._id)}>
+                </Tab>
+              ))};
+         </Tabs>
+          </Paper>
+          <Grid />
+        </Grid>
+        <Grid item lg={9} xs={6}>
+          <Paper style={styles.Country} elevation={3}>
+            {this.state.cities.map(city => (
+              <TextField label={city.name} key={city._id} />
+            ))};
+              </Paper >
+        </Grid >
+      </Grid >
     );
   }
 }
+
+
+
+
---
+++ client/src/components/Locations/testinput.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class InputField extends Component {
+ render() {
+    return (
+      <div>
+        <input></input>
+      </div>
+    )
+ }
+}
+
+export default InputField
